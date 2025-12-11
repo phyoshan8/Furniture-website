7878c53b9cdce7248be1f5345c22d591e360d887
phyoshan8 <pshan2630@gmail.com>
2025-12-11 01:39:02 +0000

    productPageFinished

---
+++ src/components/products/ProductCard.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
+import type { Product } from "@/types";
+import { Icons } from "../icons/Icon";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Link } from "react-router-dom";
+import { Button } from "../ui/button";
+
+interface ProductProps {
+  product: Product;
+}
+
+function ProductCard({ product }: ProductProps) {
+  return (
+    <Card className="size-full overflow-hidden rounded-lg py-0">
+      <CardHeader className="gap-0 border-b p-0">
+        <Link to={`porducts/${product.id}`}>
+          <AspectRatio ratio={1 / 1} className="bg-muted">
+            <img
+              src={product.images[0]}
+              alt={product.name}
+              aria-label={product.name}
+              className="size-full object-cover align-bottom"
+              loading="lazy"
+            />
+          </AspectRatio>
+        </Link>
+      </CardHeader>
+      <CardContent>
+        <CardTitle className="text-xl font-semibold">{product.name}</CardTitle>
+        <CardDescription className="text-lg">
+          <span className="font-medium">{`$ ${product.price}`}</span>
+          {product.discount > 0 && (
+            <span className="ml-6 font-extralight line-through">{`$ ${product.discount}`}</span>
+          )}
+        </CardDescription>
+      </CardContent>
+      <CardFooter>
+        {product.status === "active" ? (
+          <Button className="mb-8 w-full bg-green-950 py-6 text-lg font-bold">
+            <Icons.circlePlus className="mr-5 size-7" />
+            Add to card
+          </Button>
+        ) : (
+          <Button
+            className="mb-8 w-full bg-green-950 py-6 text-lg font-bold"
+            disabled={true}
+          >
+            Sold out
+          </Button>
+        )}
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default ProductCard;
---
+++ src/pages/products/ProductRootLayout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProductRootLayout() {
+  return <div>ProductRootLayout</div>;
+}
+
+export default ProductRootLayout;
